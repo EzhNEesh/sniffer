7fd8b8e4ceff6cd299e9b9c15a6a52e4aac6d83b
EzhNEEsh <oyaiskander@yandex.ru>
2024-07-23 18:29:10 +0300

    test: macscounter tests with desc ordering updated

--- test/test_macscounter.cpp
+++ test/test_macscounter.cpp
@@ -9,39 +9,31 @@
 
 struct MacsCounterTest : testing::Test {
     MacsCounter *macsCounter;
+    std::multimap<int, std::string> orderedMacsCountMap;
     std::unordered_map<std::string, int> macsCountExc;
-    std::multimap<int, std::string> orderedMacsExc;
+    std::vector<std::pair<std::string, int>> orderedMacsExc;
 
     void SetUp() {
         macsCounter = new MacsCounter();
 
-        macsCountExc["c8:7f:54:28:74:ac"] = 3;
-        macsCountExc["80:b6:55:60:6f:58"] = 4;
-        macsCountExc["70:c9:32:1b:54:e2"] = 13;
-        macsCountExc["52:ff:20:52:16:9a"] = 14;
-        macsCountExc["ff:ff:ff:ff:ff:ff"] = 98;
-        macsCountExc["4a:5f:99:ae:ea:99"] = 98;
-        macsCountExc["6e:52:4e:5f:f9:eb"] = 107;
-        macsCountExc["84:c5:a6:07:38:66"] = 124;
-        macsCountExc["b8:69:f4:7a:a5:93"] = 136;
-        macsCountExc["34:1c:f0:d2:78:5a"] = 5307;
-        macsCountExc["34:1c:f0:d3:40:a2"] = 5812;
-        macsCountExc["00:0c:29:65:08:ee"] = 7740;
-        macsCountExc["b8:69:f4:7a:a5:ac"] = 15235;
+        orderedMacsCountMap.insert({3, "c8:7f:54:28:74:ac"});
+        orderedMacsCountMap.insert({4, "80:b6:55:60:6f:58"});
+        orderedMacsCountMap.insert({13, "70:c9:32:1b:54:e2"});
+        orderedMacsCountMap.insert({14, "52:ff:20:52:16:9a"});
+        orderedMacsCountMap.insert({98, "ff:ff:ff:ff:ff:ff"});
+        orderedMacsCountMap.insert({98, "4a:5f:99:ae:ea:99"});
+        orderedMacsCountMap.insert({107, "6e:52:4e:5f:f9:eb"});
+        orderedMacsCountMap.insert({124, "84:c5:a6:07:38:66"});
+        orderedMacsCountMap.insert({136, "b8:69:f4:7a:a5:93"});
+        orderedMacsCountMap.insert({5307, "34:1c:f0:d2:78:5a"});
+        orderedMacsCountMap.insert({5812, "34:1c:f0:d3:40:a2"});
+        orderedMacsCountMap.insert({7740, "00:0c:29:65:08:ee"});
+        orderedMacsCountMap.insert({15235, "b8:69:f4:7a:a5:ac"});
 
-        orderedMacsExc.insert({3, "c8:7f:54:28:74:ac"});
-        orderedMacsExc.insert({4, "80:b6:55:60:6f:58"});
-        orderedMacsExc.insert({13, "70:c9:32:1b:54:e2"});
-        orderedMacsExc.insert({14, "52:ff:20:52:16:9a"});
-        orderedMacsExc.insert({98, "ff:ff:ff:ff:ff:ff"});
-        orderedMacsExc.insert({98, "4a:5f:99:ae:ea:99"});
-        orderedMacsExc.insert({107, "6e:52:4e:5f:f9:eb"});
-        orderedMacsExc.insert({124, "84:c5:a6:07:38:66"});
-        orderedMacsExc.insert({136, "b8:69:f4:7a:a5:93"});
-        orderedMacsExc.insert({5307, "34:1c:f0:d2:78:5a"});
-        orderedMacsExc.insert({5812, "34:1c:f0:d3:40:a2"});
-        orderedMacsExc.insert({7740, "00:0c:29:65:08:ee"});
-        orderedMacsExc.insert({15235, "b8:69:f4:7a:a5:ac"});
+        for (auto it = orderedMacsCountMap.rbegin(); it != orderedMacsCountMap.rend(); it++) {
+            macsCountExc[it->second] = it->first;
+            orderedMacsExc.push_back(std::pair<std::string, int> (it->second, it->first));
+        }
     }
     void TearDown() {
         delete macsCounter;
@@ -55,7 +47,7 @@ TEST_F(MacsCounterTest, GetMacsCount_Test) {
 }
 
 TEST_F(MacsCounterTest, OrderMacs_Test) {
-    std::multimap<int, std::string> orderedMacs = macsCounter->orderMacs(macsCountExc);
+    std::vector<std::pair<std::string, int>> orderedMacs = macsCounter->orderMacs(macsCountExc);
 
     ASSERT_EQ(orderedMacs, orderedMacsExc);
 }
