bc0396cc2d00fd9dcfa9ea9c9bfc0833bdfbfd6e
EzhNEEsh <oyaiskander@yandex.ru>
2024-08-05 13:41:49 +0300

    test: read macs from file

--- test/test_macscounter.cpp
+++ test/test_macscounter.cpp
@@ -3,6 +3,7 @@
 #include <unordered_map>
 #include <map>
 #include <iostream>
+#include <fstream>
 
 #include "macscounter.h"
 #include "macscounter.cpp"
@@ -16,19 +17,14 @@ struct MacsCounterTest : testing::Test {
     void SetUp() {
         macsCounter = new MacsCounter();
 
-        orderedMacsCountMap.insert({3, "c8:7f:54:28:74:ac"});
-        orderedMacsCountMap.insert({4, "80:b6:55:60:6f:58"});
-        orderedMacsCountMap.insert({13, "70:c9:32:1b:54:e2"});
-        orderedMacsCountMap.insert({14, "52:ff:20:52:16:9a"});
-        orderedMacsCountMap.insert({98, "ff:ff:ff:ff:ff:ff"});
-        orderedMacsCountMap.insert({98, "4a:5f:99:ae:ea:99"});
-        orderedMacsCountMap.insert({107, "6e:52:4e:5f:f9:eb"});
-        orderedMacsCountMap.insert({124, "84:c5:a6:07:38:66"});
-        orderedMacsCountMap.insert({136, "b8:69:f4:7a:a5:93"});
-        orderedMacsCountMap.insert({5307, "34:1c:f0:d2:78:5a"});
-        orderedMacsCountMap.insert({5812, "34:1c:f0:d3:40:a2"});
-        orderedMacsCountMap.insert({7740, "00:0c:29:65:08:ee"});
-        orderedMacsCountMap.insert({15235, "b8:69:f4:7a:a5:ac"});
+        std::fstream file("../orderedMacs.txt");
+        std::string line;
+        if (file.is_open()) {
+            while (std::getline(file, line)) {
+                size_t spacePos = line.find(' ');
+                orderedMacsCountMap.insert({std::stoi(line.substr(spacePos)), line.substr(0, spacePos)});
+            }
+        }
 
         for (auto it = orderedMacsCountMap.rbegin(); it != orderedMacsCountMap.rend(); it++) {
             macsCountExc[it->second] = it->first;
